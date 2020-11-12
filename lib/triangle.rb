6d1fd8cbc59ee57176f9c047ac5987613a21fa00
angelo milien <angelomilienjoseph@gmail.com>
2020-11-12 17:58:36 -0500

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
+
+require "pry"
+
 class Triangle
-  # write code here
+    
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    if ((@side_1 != 0 && @side_1.negative? == false) && side_1 == side_2)  &&  ((@side_2 != 0 && @side_2.negative? == false) && side_2 == side_3)  && ((@side_3 != 0 && @side_3.negative? == false) && @side_3 == @side_1)
+    "equilateral".to_sym
+    elsif ((@side_1 != 0 && @side_1.negative? == false) && @side_1 == @side_2 && @side_1 + @side_2 > @side_3) || ((@side_1 != 0 && @side_1.negative? == false) && (@side_1 == @side_3 && @side_1 + @side_3 != @side_2)) || (@side_2 != 0 && @side_2.negative? == false) && @side_2 == @side_3  
+      "isosceles".to_sym
+    elsif ((@side_1 != 0 && @side_1.negative? == false) && @side_1 != @side_2) && ((@side_2 != 0 && @side_2.negative? == false) && @side_2 != @side_3) &&  ((@side_1 != 0 && @side_1.negative? == false) && @side_1 != @side_3) && (@side_3 != 0) && (@side_2 + @side_3 > @side_1)
+      "scalene".to_sym 
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
