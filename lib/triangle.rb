b4b3cfd845dcf4aa36c4cb86b50fc2518aef063e
angelo milien <angelomilienjoseph@gmail.com>
2021-03-20 11:28:41 -0400

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
 
   def kind
     if ((@side_1 != 0 && @side_1.negative? == false) && side_1 == side_2)  &&  ((@side_2 != 0 && @side_2.negative? == false) && side_2 == side_3)  && ((@side_3 != 0 && @side_3.negative? == false) && @side_3 == @side_1)
-    "equilateral".to_sym
+      "equilateral".to_sym
     elsif ((@side_1 != 0 && @side_1.negative? == false) && @side_1 == @side_2 && @side_1 + @side_2 > @side_3) || ((@side_1 != 0 && @side_1.negative? == false) && (@side_1 == @side_3 && @side_1 + @side_3 != @side_2)) || (@side_2 != 0 && @side_2.negative? == false) && @side_2 == @side_3  
       "isosceles".to_sym
     elsif ((@side_1 != 0 && @side_1.negative? == false) && @side_1 != @side_2) && ((@side_2 != 0 && @side_2.negative? == false) && @side_2 != @side_3) &&  ((@side_1 != 0 && @side_1.negative? == false) && @side_1 != @side_3) && (@side_3 != 0) && (@side_2 + @side_3 > @side_1)
